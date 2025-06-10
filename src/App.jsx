2fb7dad0295784efdfbabdd8c00473719e114685
rafaelmoreira18 <rafafelipe9444@gmail.com>
2025-06-10 21:03:42 +0200

    first commits

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+
+import { Post } from './Post';
+
+export function App() {
+  return (
+    <div>
+   <Post author="Diego Fernandes" content="lorem impsum dolor sit amet consectetur"/>
+   <Post author="Gabriel Buzzi" content="Um novo post muito bom"/>
+    </div>
+  )
+}
+
---
+++ src/Post.jsx
@@ -0,0 +1,13 @@
+export function Post(props){
+  return (
+    <div>
+        <strong>{props.author}</strong>
+        <p>{props.content}</p>
+
+
+        
+    </div>
+
+     
+  )
+}
